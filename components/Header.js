9f5aa10b63ed63974fef08e9240917a0dfde15d6
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-15 20:32:47 -0700

    added next.connfig.js to whitelist image processing on logo.jpg

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,8 @@ function Header() {
         <div className='bg-heading_blue'>
             {/* Left */}
             <div>
-                <img className="p-5 h-30 w-140 " src="./logo.jpg" alt="daniel-james-logo" />
+                {/* <img className="p-5 h-30 w-140 " src="./logo.jpg" alt="daniel-james-logo" /> */}
+                <Image src="https://raw.githubusercontent.com/realdanieljames/portfolio/main/public/logo.jpg" width={150} height={40}/>
             </div>
 
 
