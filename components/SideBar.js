dea8b228eca9d19e6919d1653de4569a3f1f5558
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-07-15 17:50:04 -0700

    cleaner outlook

--- components/SideBar.js
+++ components/SideBar.js
@@ -3,11 +3,11 @@
 function SideBar() {
   return (
     <div className="bg-gradient-to-r from-blue-100 via-blue-200 to-pink-100
-     border-t-2 border-r-4 border-blue-900 rounded-tr-lg shadow-xl">
-      <div className="w-full border-b-2 border-blue-900 cursor-pointer ">
+     border-t-2 border-r-2 border-blue-900 rounded-tr-lg shadow-xl">
+      <div className="w-full h-auto border-b-2 border-blue-900 cursor-pointer ">
 
         {/* <img className="pr-12 py-5 w-4/6" src="/logo.png" alt="" /> */}
-        <img className="p-12 py-5 w-4/6 " src="logo3.svg" alt="" />
+        <img className=" py-5 w-4/6 h-32 " src="logo3.svg" alt="" />
       </div>
       {/* <hr /> */}
       <h1> LINKEDIN</h1>
--- data/portfolioProjects.js
+++ data/portfolioProjects.js
@@ -3,29 +3,50 @@ export default [{
         website: "https://cryptocollection.link/",
         // imageUrl: "/portfolioImages/crypto-collection-recording.mov",
         imageUrl: "https://media.giphy.com/media/iAT17I6H9XV2dYr8FB/giphy.mp4",
-        projectDescription: "This project is still one of my favorite projects. Loaded with the top 200 cryptocurrencies sorted by market capitalization. This project includes currency converter. Financial chart showing the history of selected cryptocurrency, search feature that filters people(users) and cryptos. "
+        projectDescription: `This project is still one of my favorite projects. 
+        Loaded with the top 200 cryptocurrencies sorted by market capitalization. 
+        This project includes currency converter. 
+        Financial chart showing the history of selected cryptocurrency, search feature that filters people(users) and cryptos.`
     }, {
         name: "Energy Food App",
         website: "#",
         // imageUrl: "/portfolioImages/energy-food-app.mov",
         imageUrl: "https://media.giphy.com/media/mEzGK0uhSwB6G4wSE2/source.mp4",
-        projectDescription: "This project was done with a classmate Dang Huong (Miley)(miley.dawn@codeimmersives.com). In class we were tasked with the duty to create a wireframe with our project partner. With this wireframe we will design a CRUD (Create, Read, Update, Delete) application which allows a user to add/edit/delete grocery items to their list."
+        projectDescription: `
+        This project was done with a classmate Dang Huong(Miley)(miley.dawn @codeimmersives.com).
+        In class we were tasked with the duty to create a wireframe with our project partner.
+        With this wireframe we will design a CRUD(Create, Read, Update, Delete) application which allows a user to add / edit / delete grocery items to their list.
+        `
     },
     {
         name: "Amazon Clone RED",
         website: "#",
         // imageUrl: "/portfolioImages/amazon-clone-red.mov",
         imageUrl: "https://media.giphy.com/media/XXiy9zA8MPWGNtgiUE/giphy.mp4",
-        projectDescription: "This project was done by following a tutorial on YouTube. A fellow by the name of Sonny Sangha went through an entire build and walk us(the viewers) through it. I am proud of this one because it was my second introduction on a project using Next,js and TailwindCSS. We also used FakeStoreAPI for the product content"
+        projectDescription: `
+        This project was done by following a tutorial on YouTube.
+        A fellow by the name of Sonny Sangha went through an entire build and walk us(the viewers) through it.
+        I am proud of this one because it was my second introduction on a project using Next,
+        js and TailwindCSS.
+        We also used FakeStoreAPI
+        for the product content `
     },
     {
         name: "Calories Tracker",
         website: "#",
         // imageUrl: "/portfolioImages/calories-tracker.mov",
         imageUrl: "https://media.giphy.com/media/Ci8oZUWOxgH5eQsHhS/source.mp4",
-        projectDescription: "This project was done in partnership with a team-mate by the name of Ruben Rodriguez (ruben.rodriguez@codeimmmersives.com). We were tasked with creating a project MERN stack project. A compmlete front-end and back-end compatible project. The purpose of this project is to allow signed-in users to create, update, or delete their exercises and food intake. This data is collected and instantly calculated to display to the user their total burned calories, and their total calorie intake based on the data added by the user in the add-exercise field and the add-food field. Technologies used include [ Mongo DB, Express JS, React JS, Node Js, React Redux"
-    },
-    {
+        projectDescription: `
+        This project was done in partnership with a team - mate by the name of Ruben Rodriguez(ruben.rodriguez @codeimmersives.com).
+        We were tasked to create a full MERN stack project.A complete front - end and back - end compatible project.
+        The purpose of this project is to allow signed - in users to create,
+        update,
+        or delete their exercises and food intake data.
+        This data is collected and instantly calculated to display to the user their total burned calories,
+        and their total calorie intake based on the inputted information.
+        Technologies used include[Mongo DB, Express JS, React JS, Node Js, React Redux]
+        `
+    }, {
         name: "Google Maps Job Search",
         website: "#",
         // imageUrl: "/portfolioImages/calories-tracker.mov",
